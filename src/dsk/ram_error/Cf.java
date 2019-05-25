37246ac03dd84b3c732bf760df4bd14b05fdea17
Artur <artur.m.dusinski@gmail.com>
2019-05-25 15:29:00 +0200

    Dodano blad CF.

---
+++ src/dsk/ram_error/Cf.java
@@ -0,0 +1,50 @@
+package dsk.ram_error;
+
+import dsk.Ram;
+
+public class Cf extends ErrorTemplate{
+	enum CfType{
+		CF_IN, CF_ID_0, CF_ID_1, CF_D
+	}
+	
+	private CfType cfType;
+	private int pairX;
+	private int pairY;
+	
+	public Cf(Ram ram, int x, int y, int pairX, int pairY, CfType cfType) {
+		super(ram, x, y);
+		this.pairX = pairX;
+		this.pairY = pairY;
+	}
+
+	@Override
+	public boolean getRam(int x, int y) {
+		if(cfType == CfType.CF_D) {
+			ram.setRam(pairX, pairY, ram.getRam(pairX, pairY));
+		}
+		return ram.getRam(x, y);
+	}
+
+	@Override
+	public void setRam(int x, int y, boolean value) {
+		ram.setRam(x, y, value);
+		switch (cfType) {
+		case CF_IN:
+			ram.setRam(pairX, pairY, ram.getRam(pairX, pairY));
+			break;
+		case CF_ID_0:
+			ram.setRam(pairX, pairY, false);
+			break;
+		case CF_ID_1:
+			ram.setRam(pairX, pairY, true);
+			break;
+		case CF_D:
+			ram.setRam(pairX, pairY, ram.getRam(pairX, pairY));
+			break;
+		}
+		
+	}
+
+
+
+}
